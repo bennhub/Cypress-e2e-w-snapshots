71905fe5314ed7b5a7a1771d2bd6e842c28d3dbf
ben <happyb3no@gmail.com>
2020-02-14 15:58:30 -0800

    updated FIFA bundles

---
+++ integration/FIFA/Bundles/BundlePDP/Int75Boost.js
@@ -0,0 +1,35 @@
+describe('Bundles_Compass', function() {
+    it('Gets, types and visual tests', function() {
+
+
+//Visits Home and begins to qual
+cy.visit('/')
+cy.contains('Get started').click()
+
+//Quals Compass address
+cy.get('#autocomplete-form-address-google')
+        .type(Cypress.env('address_FIFA'))
+  
+cy.contains('Check availability').click()
+         .wait(7000) 
+
+
+//Bundle PDP FIFA Qual
+
+//******Bundles Optik 7-1 + Int 150 PDP
+cy.visit('/product/internet-75-boost?bundle&variant=9150529041113486764|term,9150280421713159508')
+
+cy.setCookie('QSI_SI_8II6mHwhblM7icZ_intercept','true' )
+.wait(3000)
+
+//Opens T & C section
+cy
+.contains('View terms and conditions')
+.should('be.visible', { timeout: 5000 })
+.click()
+
+//snapshots page for comparison
+cy.matchImageSnapshot("Internet 75 + Boost PDP");
+
+})
+})
