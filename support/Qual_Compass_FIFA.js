798fbb44236922473ec79cd47c0ce68cbf49468e
ben <happyb3no@gmail.com>
2020-10-29 10:32:06 -0700

    adding latest changes

--- support/Qual_Compass_FIFA.js
+++ support/Qual_Compass_FIFA.js
@@ -2,9 +2,9 @@
 
 //Manual Compass Address FIll
 Cypress.Commands.add('fillCompassAddress', () => {
- //cy.visit('/shop/eligibility')//Qual Compass
- //  .wait(4000)
- cy.get('#autocomplete-form-address-google', {force: true})
+  cy.get('[data-qa="button-addressHeader"]',{ timeout: 15000 }).first().click({force: true})
+ //cy.get('#autocomplete-form-address-google', {force: true})
+ cy.get('#street-address',{ timeout: 60000 }).click({force: true})
    .type(Cypress.env('address_150'), {force: true})
  cy.get('#autocomplete-form-submit', {force: true}).click()
    .wait(4000) 
@@ -12,7 +12,9 @@ Cypress.Commands.add('fillCompassAddress', () => {
 
 //Manual FIFA Address Fill
 Cypress.Commands.add('fillFifaAddress', () => {
-  cy.get('#autocomplete-form-address-google', {timeout: 7000})
+  cy.get('[data-qa="button-addressHeader"]',{ timeout: 15000 }).first().click({force: true})
+  //cy.get('#autocomplete-form-address-google', {timeout: 7000})
+  cy.get('#street-address',{ timeout: 60000 }).click({force: true})
     .type(Cypress.env('address_FIFA'))
   cy.get('#autocomplete-form-submit').click()
     .wait(4000)
